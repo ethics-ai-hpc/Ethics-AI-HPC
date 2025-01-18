89cb7a1f7f4368ec70ce686d520c7f4ae5acf022
Pinhuan Wang <36356117+Joeyw02@users.noreply.github.com>
2025-01-18 15:57:09 -0500

    Update scripts.js

--- src/ scripts /scripts.js
+++ src/ scripts /scripts.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("网站已加载");
+    const links = document.querySelectorAll('nav ul li a');
+    const sections = document.querySelectorAll('section');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            sections.forEach(section => {
+                section.style.display = 'none';
+            });
+            links.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector(this.getAttribute('href')).style.display = 'block';
+            this.classList.add('active');
+        });
+    });
 });
