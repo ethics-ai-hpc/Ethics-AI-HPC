13bb2783a3b1b3dd6fd21363834ec22b1b3d6ea4
Pinhuan Wang <36356117+Joeyw02@users.noreply.github.com>
2025-01-18 16:37:22 -0500

    Update scripts.js

--- src/ scripts /scripts.js
+++ src/ scripts /scripts.js
@@ -5,13 +5,20 @@ document.addEventListener('DOMContentLoaded', function() {
     links.forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault();
+            const targetSection = document.querySelector(this.getAttribute('href')); // 确保获取正确的目标区块
+
+            // 隐藏所有section
             sections.forEach(section => {
                 section.style.display = 'none';
             });
+
+            // 移除所有链接的active类
             links.forEach(link => {
                 link.classList.remove('active');
             });
-            document.querySelector(this.getAttribute('href')).style.display = 'block';
+
+            // 显示点击的链接对应的section并添加active类
+            targetSection.style.display = 'block';
             this.classList.add('active');
         });
     });
